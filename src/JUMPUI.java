e731ddbf67832b35d2cf22cc6673c949eb7d6641
hernanre <hernanre@rose-hulman.edu>
2021-04-26 20:27:12 -0400

    Added basic JFrame

--- src/JUMPUI.java
+++ src/JUMPUI.java
@@ -1,6 +1,11 @@
+import javax.swing.*;
+
 //Authors: Ricardo H, Carlos F, and Cehong W.
 public class JUMPUI {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        JFrame frame = new JFrame("FrameDemo");
+        frame.setVisible(true);
     }
 }
