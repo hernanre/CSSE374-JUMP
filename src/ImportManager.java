18d67581d452ce417f9d0bb5c93cf0d3a7cb2605
hernanre <hernanre@rose-hulman.edu>
2021-05-03 19:18:09 -0400

    Made the Package Compiler Class and made changes to the import manager

--- src/ImportManager.java
+++ src/ImportManager.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -13,11 +14,13 @@ public class ImportManager {
 
     public ImportManager() {
         this.file = new File("src/JUMPS-data-example.xlsx");
-        this.importSupplies();
-        this.importComponents();
+
+
+//        this.importCommands();
     }
 
-    public boolean importSupplies () {
+    public HashMap<String, Supply> importSupplies () {
+        HashMap<String, Supply> supplies = new HashMap<>();
         try {
             System.out.println("I am importing the supplies of the system!!");
             System.out.println("");
@@ -42,18 +45,22 @@ public class ImportManager {
                     int quantityOriginal =  (int) cellIterator.next().getNumericCellValue();
                     String type = cellIterator.next().getStringCellValue();
                     String unit = cellIterator.next().getStringCellValue();
-                    System.out.println(name + ", " + quantityAvailable + ", " + quantityOriginal + ", " + type + ", " + unit);
+                    Supply s =  new Supply(name, quantityAvailable, quantityOriginal, type, unit);
+//                    System.out.println(s.toString());
+                    supplies.put(name, s);
+//                    System.out.println(name + ", " + quantityAvailable + ", " + quantityOriginal + ", " + type + ", " + unit);
                 }
             }
             wb.close();
             System.out.println("");
-            return true;
+            return supplies;
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
+            return supplies;
         }
     }
-    public boolean importComponents () {
+    public HashMap<String, Component> importComponents () {
+        HashMap<String, Component> components = new HashMap<>();
         try {
             System.out.println("I am importing the Components of the system!!");
             System.out.println("");
@@ -80,10 +87,57 @@ public class ImportManager {
                     String name =  cellIterator.next().getStringCellValue();
                     String description = cellIterator.next().getStringCellValue();
                     String status = cellIterator.next().getStringCellValue();
-                    System.out.println(name + " is a " + type + " with ID: " + id + " and is currently " + status + ".\n Here is a description of it: " + description);
+                    Component c = new Component(type, id, name, description,status);
+                    components.put(name, c);
+//                    System.out.println(c.toString());
+//                    System.out.println(name + " is a " + type + " with ID: " + id + " and is currently " + status + ".\n Here is a description of it: " + description);
                 }
             }
+
+            wb.close();
+            return components;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return components;
+    }
+    public boolean importCommands () {
+        try {
+            System.out.println("I am importing the commands of the system!");
             System.out.println("");
+            FileInputStream fis = new FileInputStream(this.file);   //obtaining bytes from the file
+            //creating Workbook instance that refers to .xlsx file
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+
+            //Sheet in excel file is labeled as Capabilities but they're the Components of the UAV
+            XSSFSheet sheet = wb.getSheet("Command-actions");     //creating a Sheet object to retrieve object
+            Iterator<Row> itr = sheet.iterator();    //iterating over excel file
+            while (itr.hasNext()) {
+                Row row = itr.next();
+                if(row.getRowNum() == 0){
+                    continue;
+                }
+                Iterator<Cell> cellIterator = row.cellIterator();
+
+                while (cellIterator.hasNext()) {
+                    String commandID = cellIterator.next().getStringCellValue();
+                    if (commandID.isEmpty()) {
+                        break;
+                    }
+                    String commandName =  cellIterator.next().getStringCellValue();
+                    String parameters = "";
+                    if(cellIterator.hasNext()) {
+                        parameters = cellIterator.next().getStringCellValue();
+                    }
+                    else{
+                        parameters = "NONE";
+                    }
+
+                    System.out.println(commandID + " is the command " + commandName + " with parameters: " + parameters);
+                }
+            }
+
             wb.close();
             return true;
         } catch (IOException e) {
