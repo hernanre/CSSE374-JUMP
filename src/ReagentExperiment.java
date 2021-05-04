d7932887ad5af39e2de03a9c9999f728f6ff4ca3
wangc7@rose-hulman.edu <Wch000723>
2021-05-04 17:48:44 -0400

    ReagentExperiment

--- src/ReagentExperiment.java
+++ src/ReagentExperiment.java
@@ -1,4 +1,30 @@
-public class ReagentExperiment {
+public class ReagentExperiment implements Experiment {
 
 
+    private String reagentName, detail, measurement, ID, name;
+    private int quantity, time;
+
+    public ReagentExperiment(String reagentName, String ID, String name,
+                             String detail, String measurement, int quantity, int time){
+        this.reagentName = reagentName;
+        this.detail = detail;
+        this.quantity = quantity;
+        this.time = time;
+        this.measurement= measurement;
+    }
+
+    @Override
+    public void updateStatus() {
+
+    }
+
+    @Override
+    public int getPriority() {
+        return 0;
+    }
+
+    @Override
+    public String getLabel() {
+        return null;
+    }
 }
