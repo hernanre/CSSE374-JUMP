5267f9073e986012994835585ff2d9c20dcd3574
hernanre <hernanre@rose-hulman.edu>
2021-05-03 21:59:31 -0400

    add toJSONObject() in the Sample Experiment, create JSON file in the Package Compiler, and had to re-add the excel file because it wasnt importing data.

--- src/PackageCompiler.java
+++ src/PackageCompiler.java
@@ -1,2 +1,29 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class PackageCompiler {
+    public void toJSONfile(ArrayList<JSONObject> experiments) throws JSONException, IOException {
+        JSONObject jSON = new JSONObject();
+
+        JSONObject payload = new JSONObject();
+        for(int i = 0; i < experiments.size(); i++){
+            payload.put("experiment"+(i+1), experiments.get(i));
+        }
+        jSON.put("payload", payload);
+        FileWriter file = new FileWriter("src/File.json");
+        file.write(jSON.toString(1));
+//       file.append(jSON.toString(1));
+        file.close();
+    }
+
+
+
+
+
 }
--- src/SampleExperiment.java
+++ src/SampleExperiment.java
@@ -1,3 +1,7 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashSet;
 
 public class SampleExperiment implements Experiment{
@@ -55,7 +59,36 @@ public class SampleExperiment implements Experiment{
     public String getLabel() {
         return "Sample Ex: " + sampleName + " of " + quantity + " " + unit;
     }
+    public JSONObject toJSONObject() throws JSONException {
+        JSONObject experimentInfo = new JSONObject();
+        experimentInfo.put("experiment_id", "TESTID");
+        experimentInfo.put("experiment_name","TESTName");
+        experimentInfo.put("experiment_type", "Sample-Only");
 
+        JSONArray commands = new JSONArray();
+
+        JSONObject command1 = new JSONObject();
+        command1.put("command", "C19");
+
+        JSONObject commandFirstHalf = new JSONObject();
+        JSONObject command2 = new JSONObject();
+
+        String params[] = new String[1];
+        params[0] = "HPGC-1";
+        command2.put("param", params);
+        command2.put("command", "C15");
+
+
+//       JSONWriter
+        JSONObject command3 = new JSONObject();
+        command3.put("command", "C19");
+
+        commands.put(command1);
+        commands.put(command2);
+        commands.put(command3);
+        experimentInfo.put("experiment_commands", commands);
+        return experimentInfo;
+    }
     }
 
 
