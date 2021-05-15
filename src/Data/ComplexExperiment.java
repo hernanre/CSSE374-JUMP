b83f7e749d38513332d1e9bf9e775f8160e0e8c0
hernanre <hernanre@rose-hulman.edu>
2021-05-15 16:07:20 -0400

    implemented JSON method

--- src/Data/ComplexExperiment.java
+++ src/Data/ComplexExperiment.java
@@ -1,5 +1,6 @@
 package Data;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
@@ -46,7 +47,45 @@ public class ComplexExperiment implements Experiment {
 
     @Override
     public JSONObject getJSONObject() {
-        return null;
+        JSONObject experimentInfo = new JSONObject();
+        experimentInfo.put("experiment_id", this.getID());
+        experimentInfo.put("experiment_name","Test Complex Experiment");
+        experimentInfo.put("experiment_type", "Complex");
+
+        System.out.println(this.commandList.toString());
+        JSONArray commands = new JSONArray();
+
+//        JSONObject command1 = new JSONObject();
+//        command1.put("command", "C19");
+//
+//
+//        ArrayList<String> params = new ArrayList<>();
+//        params.add("HPGC-1");
+//
+//
+////       JSONWriter
+//        JSONObject command2 = new JSONObject();
+//        command2.put("command", "C8");
+//
+//
+//        JSONObject command3 = new JSONObject();
+//        command3.put("command", "C4");
+//        JSONObject command4 = new JSONObject();
+//
+//        //this adds all of the commands that require the reagents name and quantity.
+//        for(int i = 0; i < this.reagents.size(); i++){
+//            commands.add(this.reagents.get(i).getJSONReagentCommand());
+//        }
+//
+//        command4.put("param", params.toString());
+//        command4.put("command", "C15");
+//        JSONObject command5 = new JSONObject();
+//        command5.put("command", "C7");
+        for(int i = 0; i < this.commandList.size(); i++) {
+            commands.add(this.commandList.get(i).toJson());
+        }
+        experimentInfo.put("experiment_commands", commands);
+        return experimentInfo;
     }
 
     public ArrayList<Command> getCommandList() {
